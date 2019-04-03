3d5b3fe087dc5da43b20caf798305c7dda85276b
Maxim Umblia <umblia34@gmail.com>
2019-04-03 15:10:21 +0300

    Basic version of translator

--- build/js/main.js
+++ build/js/main.js
@@ -2,70 +2,68 @@ window.onload = () => {
   getLangs(
     `https://translate.yandex.net/api/v1.5/tr.json/getLangs?ui=en&key=trnsl.1.1.20190402T120324Z.f79353dd063ad3d1.a87fa23856d925eedcad3be1d14a6cd7e59bc8b2`
   );
-
-  let firstLang = document.querySelector("#firstLang");
-  let firstLangValues = firstLang.options;
-  firstLangValues.forEach(option => {
-    console.log(option);
-  });
-
-  let textToTranslate = "";
-  let input = document.querySelector(".input");
-  let result = document.querySelector(".result");
-  let resultText = "";
   let button = document
     .querySelector(".btn-translate")
     .addEventListener("click", () => {
+      let input = document.querySelector(".input");
+
       textToTranslate = input.value;
       if (textToTranslate === "" || textToTranslate === undefined) {
         alert("Please enter word that need to be translate");
       } else {
+        //Call function to translate
         translate(
-          `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190402T120324Z.f79353dd063ad3d1.a87fa23856d925eedcad3be1d14a6cd7e59bc8b2&lang=en-ru&text=${textToTranslate}&format=plain`
+          `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190402T120324Z.f79353dd063ad3d1.a87fa23856d925eedcad3be1d14a6cd7e59bc8b2&lang=${
+            getLangValues().firstKey
+          }-${getLangValues().secondKey}&text=${textToTranslate}&format=plain`
         );
       }
     });
   function translate(url) {
-    let xhr = new XMLHttpRequest();
-    // let token = 'trnsl.1.1.20190402T120324Z.f79353dd063ad3d1.a87fa23856d925eedcad3be1d14a6cd7e59bc8b2';
-
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState == 4 && xhr.status === 200) {
-        let json = JSON.parse(xhr.response);
-        let resultText = json["text"];
+    let textToTranslate = "";
+    let result = document.querySelector(".result");
+    fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonResponse => {
+        let resultText = jsonResponse["text"];
         result.innerHTML = resultText;
-      } else if (xhr.status === 400 || xhr.status === 403) {
-        alert("Request error");
-        xhr.abort();
-      }
-    };
-    xhr.open("get", url);
+      });
+  }
+  //Get langs keys from options
+  function getLangValues() {
+    let firstLang = document.querySelector("#firstlang");
+    let secondLang = document.querySelector("#secondlang");
 
-    xhr.send();
+    let keys = {
+      firstKey: "",
+      secondKey: ""
+    };
+    //Keys like [az-ru]
+    keys.firstKey = firstLang.value;
+    keys.secondKey = secondLang.value;
+    return keys;
   }
 
+  //Put all langs to select element
   function getLangs(url) {
-    let xhr = new XMLHttpRequest();
-    // let token = 'trnsl.1.1.20190402T120324Z.f79353dd063ad3d1.a87fa23856d925eedcad3be1d14a6cd7e59bc8b2';
-
-    xhr.onload = () => {
-      if (xhr.status === 200) {
-        let json = JSON.parse(xhr.responseText);
-        let keys = Object.keys(json["langs"]);
+    fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonResponse => {
+        let langs = Object.keys(jsonResponse["langs"]);
         let selects = document.querySelectorAll("select");
         selects.forEach(select => {
-          keys.forEach(key => {
+          langs.forEach(key => {
+            let value = jsonResponse["langs"][key];
             let option = document.createElement("option");
             option.value = key;
-            option.innerHTML = key;
+            option.innerHTML = value;
             select.appendChild(option);
           });
         });
-      } else {
-      }
-    };
-    xhr.open("get", url);
-
-    xhr.send();
+      });
   }
 };
